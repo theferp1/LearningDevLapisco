dc76e7585318c5596d07cd625e9d97400f1b955a
Nicolas Primo <nicolasprimo@gmail.com>
2023-08-15 17:18:46 -0300

    fixing wallpaper responsivity

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -5,11 +5,11 @@ const Login = () => {
 
   return (
     <div className="w-screen h-screen bg-slate-400 flex flex-col items-center justify-center ">
-      <div className="z-[15] flex flex-col mb-44 gap-20 items-center justify-center">
+      <div className="z-[15] flex flex-col mb-10  lg:mb-44 gap-20 items-center justify-center">
         <img
           src="/WhiteLapiscoLogo.svg"
           alt=""
-          className={` w-[250px]  focus:animate-spin `}
+          className={` w-[170px] md:w-[250px]  focus:animate-spin `}
         />
         <form
           action=""
@@ -30,7 +30,7 @@ const Login = () => {
                 setBlur(false);
               }
             }}
-            className="w-[366px] rounded-[20px] h-[65px] px-5 bg-[#3E3E3E]  bg-opacity-50 backdrop-blur-md placeholder-white text-white text-xl font-medium focus:outline-none focus:ring-2 focus:ring-[#BCBCBC]"
+            className="w-[266px] h-[55px] md:w-[366px] rounded-[20px] md:h-[65px] px-5 bg-[#3E3E3E]  bg-opacity-50 backdrop-blur-md placeholder-white text-white text-md md:text-xl font-medium focus:outline-none focus:ring-2 focus:ring-[#BCBCBC]"
           />
           <input
             type="password"
@@ -47,9 +47,9 @@ const Login = () => {
                 setBlur(false);
               }
             }}
-            className="w-[366px] rounded-[20px] h-[65px] px-5 bg-[#3E3E3E] bg-opacity-50 backdrop-blur-md placeholder-white text-white text-xl font-medium focus:outline-none focus:ring-2 focus:ring-[#BCBCBC]"
+            className="w-[266px] h-[55px] md:w-[366px] rounded-[20px] md:h-[65px] px-5 bg-[#3E3E3E] bg-opacity-50 backdrop-blur-md placeholder-white text-white text-md md:text-xl font-medium focus:outline-none focus:ring-2 focus:ring-[#BCBCBC]"
           />
-          <button className="text-xl bg-white text-black font-bold w-fit px-12 hover:bg-slate-200 h-[65px] rounded-[20px]">
+          <button className="text-md md:text-xl bg-white text-black font-bold w-fit px-7 md:px-12 hover:bg-slate-200 h-[50px] md:h-[65px] rounded-[20px]">
             Entrar
           </button>
         </form>
@@ -59,7 +59,7 @@ const Login = () => {
       <img
         src="/WalppaperLoginTransformed.png"
         alt=""
-        className={`absolute lg:w-screen lg:h-screen z-0 transition-all duration-500 ${
+        className={`absolute w-[640px] h-screen sm:w-[768px] sm:h-screen md:w-[1024px] object-cover md:h-screen lg:w-screen lg:h-screen z-0 transition-all duration-500 ${
           blur ? "blur-lg" : "blur-0"
         }`}
       />
